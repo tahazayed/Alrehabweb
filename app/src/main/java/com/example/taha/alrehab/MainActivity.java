e127aeb3fb17c2967a38e1899aa46f99fe9bc3e2
taha zayed <tahazayed@gmail.com>
2016-01-02 01:40:06 +0200

    removing cache

--- app/src/main/java/com/example/taha/alrehab/MainActivity.java
+++ app/src/main/java/com/example/taha/alrehab/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -44,7 +45,7 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
             mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     //| View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    // | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
@@ -129,6 +130,7 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
         Intent intent = new Intent(this, NotificationsService.class);
         startService(intent);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
     protected void RefreshPage()

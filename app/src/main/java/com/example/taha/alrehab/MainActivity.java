b4f6223cafbff0f6d7ee953916f9fe6fd0ab1a8b
taha zayed <tahazayed@gmail.com>
2016-01-14 20:29:47 +0200

    removing stay on

--- app/src/main/java/com/example/taha/alrehab/MainActivity.java
+++ app/src/main/java/com/example/taha/alrehab/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
             mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
                     //| View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    // | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
@@ -76,11 +76,11 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         webSettings.setDisplayZoomControls(true);
         webSettings.setDomStorageEnabled(true);
         webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
-        webSettings.setLoadsImagesAutomatically(true);
+        webSettings.setLoadWithOverviewMode(true);
 
 
         browser.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        browser.setKeepScreenOn(true);
+        browser.setKeepScreenOn(false);
 
 
         WebViewClientImpl webViewClient = new WebViewClientImpl(this);

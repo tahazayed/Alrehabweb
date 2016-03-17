0e5567fae2fcc667f36dd64942e1900f23580f76
taha zayed <tahazayed@gmail.com>
2016-03-17 18:49:43 +0200

    splash screen wait for 5 seconds

--- app/src/main/java/com/taha/alrehab/SplashScreen.java
+++ app/src/main/java/com/taha/alrehab/SplashScreen.java
@@ -3,6 +3,7 @@ package com.taha.alrehab;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.Window;
 import android.view.WindowManager;
@@ -26,42 +27,56 @@ public class SplashScreen extends Activity {
 
         setContentView(R.layout.splashscreen);
 
-        if (ConnectionHelper.isOnline()) {
-            new Handler().postDelayed(new Runnable() {
+        new CountDownTimer(5000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // do something after 1s
+            }
+
+            @Override
+            public void onFinish() {
+                if (ConnectionHelper.isOnline()) {
+                    new Handler().postDelayed(new Runnable() {
 
 
-                @Override
-                public void run() {
-                    // TODO Auto-generated method stub
-                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
-                    startActivity(i);
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                            startActivity(i);
 
 
-                    //jeda selesai Splashscreen
-                    this.finish();
+                            //jeda selesai Splashscreen
+                            this.finish();
+                        }
+
+                        private void finish() {
+                            // TODO Auto-generated method stub
+
+                        }
+                    }, splashInterval);
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "no internet", Toast.LENGTH_LONG).show();
+                    Thread thread = new Thread() {
+                        @Override
+                        public void run() {
+                            try {
+                                Thread.sleep(3500); // As I am using LENGTH_LONG in Toast
+                                System.exit(0);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    };
+                    thread.start();
                 }
+            }
 
-                private void finish() {
-                    // TODO Auto-generated method stub
+        }.start();
 
-                }
-            }, splashInterval);
 
-        } else {
-            Toast.makeText(getApplicationContext(), "no internet", Toast.LENGTH_LONG).show();
-            Thread thread = new Thread() {
-                @Override
-                public void run() {
-                    try {
-                        Thread.sleep(3500); // As I am using LENGTH_LONG in Toast
-                        System.exit(0);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-            thread.start();
-        }
     }
 
 
